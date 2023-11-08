c75df9dcd492d3e64d408b342d550b637b1a24ba
chris1424 <146835016+chris1424@users.noreply.github.com>
2023-11-09 00:53:10 +0200

    Update collatz.c

--- collatz/src/collatz.c
+++ collatz/src/collatz.c
@@ -12,15 +12,15 @@ unsigned long collatz(unsigned long n) {
     }
     return length;
 }
-
+//make faction that does the collatz with a variable
 int main() {
      long start, end, num, max_length = 0, max_num = 0;
-
-    printf("Εισαγάγετε τον ελάχιστο ακέραιο του εύρους: ");
+//initialize mag_length and max_num ,variables we will need in for 
+    printf("Εισαγάγετε πρωτο ακεραιο εύρους: ");
     scanf("%ld", &start);
-    printf("Εισαγάγετε τον μέγιστο ακέραιο του εύρους: ");
+    printf("Εισαγάγετε δευτερο ακεραιο εύρους: ");
     scanf("%ld",&end); 
-    
+    //scans user's type while we are asking him for two intigers
     
     
    if(start<1){
@@ -30,6 +30,7 @@ int main() {
 
         printf("0.\n");}
     else
+    //created this if in case the user typed 0 or a smaller number
    {
     for (num = start; num <= end; num++) {
         unsigned long length = collatz(num);
@@ -38,12 +39,12 @@ int main() {
             max_length = length;
             max_num = num;
         }
-    
+    //in case none of the variables given are 0 or smaller the for finds the number with the biggest length, once it has checked ecery single intiger between the 2 the user typed
    }
    }
     if (start>0){
         if (end>0){
-    
+    //this is in case one of the intigers are 0 or smaller so it wont print the following ,also if there isn't any mistakes it will print it correctly
     printf("Ο αριθμός με το μέγιστο μήκος ακολουθίας Collatz στο εύρος %ld -  %ld είναι %ld με μήκος %ld.\n", start, end, max_num, max_length);
 
     return 0;
